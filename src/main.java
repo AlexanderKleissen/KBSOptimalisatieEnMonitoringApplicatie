5bd4e04686a606b8dcb44bf8a7c71f38e39aacf9
EstherTomasouw <74213306+EstherTomasouw@users.noreply.github.com>
2021-04-19 13:49:54 +0200

    weer verwijderen

--- src/main.java
+++ src/main.java
@@ -1,7 +1,6 @@
 public class main {
     public static void main(String[] args) {
         MonitoringFrame monitoringFrame = new MonitoringFrame();
-        System.out.println("test");
     }
     // test
 }
