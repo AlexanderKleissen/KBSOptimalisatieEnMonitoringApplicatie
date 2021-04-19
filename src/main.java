d2005650143bc2100d13dfd9354fab23667b3a5d
EstherTomasouw <74213306+EstherTomasouw@users.noreply.github.com>
2021-04-19 13:48:46 +0200

    testen

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
     public static void main(String[] args) {
         MonitoringFrame monitoringFrame = new MonitoringFrame();
+        System.out.println("test");
     }
     // test
 }
